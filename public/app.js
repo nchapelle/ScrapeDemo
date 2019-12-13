b23c304e0628aadda2aaf2afd10ec555a57d12f9
Nolan Chapelle <53437031+nchapelle@users.noreply.github.com>
2019-12-13 14:10:04 -0500

    prettier

--- public/app.js
+++ public/app.js
@@ -3,10 +3,17 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < data.length; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
+    $("#articles").append(
+      "<p data-id='" +
+        data[i]._id +
+        "'>" +
+        data[i].title +
+        "<br />" +
+        data[i].link +
+        "</p>"
+    );
   }
 });
-
 
 // Whenever someone clicks a p tag
 $(document).on("click", "p", function() {
@@ -30,7 +37,9 @@ $(document).on("click", "p", function() {
       // A textarea to add a new note body
       $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
       // A button to submit a new note, with the id of the article saved to it
-      $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
+      $("#notes").append(
+        "<button data-id='" + data._id + "' id='savenote'>Save Note</button>"
+      );
 
       // If there's a note in the article
       if (data.note) {
